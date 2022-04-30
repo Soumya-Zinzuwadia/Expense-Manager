7d2304ff6c1b0546e2eb201318c8a18068206686
Soumya Zinzuwadia <soumya@whozzat.com>
2022-04-30 19:27:55 +0530

    Soumya| Expenses List

---
+++ src/components/Charts/Chart.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => {
+    return dataPoint.value;
+  });
+  const totalMaximum = Math.max(...dataPointValues);
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          max={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
